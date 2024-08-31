35d04e13536e1526825b9648ac46283bf79b84eb
Takashi Yamashina <takashi.yamashina@gmail.com>
2024-08-31 22:49:02 +0900

    first commit

---
+++ postgres/00-max-connections.sql
@@ -0,0 +1,2 @@
+-- ALTER SYSTEM SET idle_in_transaction_session_timeout = '1min';
+ALTER SYSTEM SET max_connections = 100;
---
+++ src/main/resources/db/changelog/changelogs/03-insert-sample-users.sql
@@ -0,0 +1,3 @@
+insert into users (id, name, email, age) values (1, 'John Doe', 'john.doe@example.com', 30);
+insert into users (id, name, email, age) values (2, 'Jane Doe', 'jane.doe@example.com', 25);
+insert into users (id, name, email, age) values (3, 'Alice Smith', 'alice.smith@example.com', 35);
